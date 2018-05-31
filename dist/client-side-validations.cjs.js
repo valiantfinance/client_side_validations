0e697e77582a412af8cb0a875ec7245c6b37b48a
Geremia Taglialatela <tagliala.dev@gmail.com>
2018-05-31 14:46:12 +0200

    Improve uniqueness validator

--- dist/client-side-validations.cjs.js
+++ dist/client-side-validations.cjs.js
@@ -498,30 +498,38 @@ ClientSideValidations.validators.local.uniqueness = function (element, options) 
       valid = void 0,
       value = void 0;
   name = element.attr('name');
-  if (/_attributes\]\[\d/.test(name)) {
-    matches = name.match(/^(.+_attributes\])\[\d+\](.+)$/);
-    namePrefix = matches[1];
-    nameSuffix = matches[2];
-    value = element.val();
-    if (namePrefix && nameSuffix) {
-      form = element.closest('form');
-      valid = true;
-      form.find(':input[name^="' + namePrefix + '"][name$="' + nameSuffix + '"]').each(function () {
-        if ($(this).attr('name') !== name) {
-          if ($(this).val() === value) {
-            valid = false;
-            return $(this).data('notLocallyUnique', true);
-          } else {
-            if ($(this).data('notLocallyUnique')) {
-              return $(this).removeData('notLocallyUnique').data('changed', true);
-            }
-          }
+
+  if (!/_attributes\]\[\d/.test(name)) {
+    return;
+  }
+
+  matches = name.match(/^(.+_attributes\])\[\d+\](.+)$/);
+  namePrefix = matches[1];
+  nameSuffix = matches[2];
+  value = element.val();
+
+  if (!(namePrefix && nameSuffix)) {
+    return;
+  }
+
+  form = element.closest('form');
+  valid = true;
+
+  form.find(':input[name^="' + namePrefix + '"][name$="' + nameSuffix + '"]').each(function () {
+    if ($(this).attr('name') !== name) {
+      if ($(this).val() === value) {
+        valid = false;
+        return $(this).data('notLocallyUnique', true);
+      } else {
+        if ($(this).data('notLocallyUnique')) {
+          return $(this).removeData('notLocallyUnique').data('changed', true);
         }
-      });
-      if (!valid) {
-        return options.message;
       }
     }
+  });
+
+  if (!valid) {
+    return options.message;
   }
 };
 
--- src/validators/local/uniqueness.js
+++ src/validators/local/uniqueness.js
@@ -4,29 +4,37 @@ import ClientSideValidations from '../../ClientSideValidations'
 ClientSideValidations.validators.local.uniqueness = function (element, options) {
   let form, matches, name, namePrefix, nameSuffix, valid, value
   name = element.attr('name')
-  if (/_attributes\]\[\d/.test(name)) {
-    matches = name.match(/^(.+_attributes\])\[\d+\](.+)$/)
-    namePrefix = matches[1]
-    nameSuffix = matches[2]
-    value = element.val()
-    if (namePrefix && nameSuffix) {
-      form = element.closest('form')
-      valid = true
-      form.find(':input[name^="' + namePrefix + '"][name$="' + nameSuffix + '"]').each(function () {
-        if ($(this).attr('name') !== name) {
-          if ($(this).val() === value) {
-            valid = false
-            return $(this).data('notLocallyUnique', true)
-          } else {
-            if ($(this).data('notLocallyUnique')) {
-              return $(this).removeData('notLocallyUnique').data('changed', true)
-            }
-          }
+
+  if (!/_attributes\]\[\d/.test(name)) {
+    return
+  }
+
+  matches = name.match(/^(.+_attributes\])\[\d+\](.+)$/)
+  namePrefix = matches[1]
+  nameSuffix = matches[2]
+  value = element.val()
+
+  if (!(namePrefix && nameSuffix)) {
+    return
+  }
+
+  form = element.closest('form')
+  valid = true
+
+  form.find(':input[name^="' + namePrefix + '"][name$="' + nameSuffix + '"]').each(function () {
+    if ($(this).attr('name') !== name) {
+      if ($(this).val() === value) {
+        valid = false
+        return $(this).data('notLocallyUnique', true)
+      } else {
+        if ($(this).data('notLocallyUnique')) {
+          return $(this).removeData('notLocallyUnique').data('changed', true)
         }
-      })
-      if (!valid) {
-        return options.message
       }
     }
+  })
+
+  if (!valid) {
+    return options.message
   }
 }
