bc65406fc7ae2f97b5f08e5bcae1a757ea57d0eb
Geremia Taglialatela <tagliala.dev@gmail.com>
2018-06-02 12:06:28 +0200

    Use new version number and name for JavaScript
    Since this package is also going to be published on npm, we need another
    version number and name for the JavaScript asset

--- rollup.config.js
+++ rollup.config.js
@@ -3,31 +3,15 @@ import commonjs from 'rollup-plugin-commonjs'
 import copy from 'rollup-plugin-copy'
 import resolve from 'rollup-plugin-node-resolve'
 import pkg from './package.json'
-import fs from 'fs'
-import path from 'path'
 
-const versionPath = path.join(__dirname, 'lib', 'client_side_validations', 'version.rb')
-const version = fs.readFileSync(versionPath, 'utf8').match(/'[\d.]*'/gm).join().replace(/'/g, '')
 const year = new Date().getFullYear()
+
 const banner = `/*!
- * Client Side Validations - v${version} (https://github.com/DavyJonesLocker/client_side_validations)
+ * Client Side Validations JS - v${pkg.version} (https://github.com/DavyJonesLocker/client_side_validations)
  * Copyright (c) ${year} Geremia Taglialatela, Brian Cardarella
  * Licensed under MIT (http://opensource.org/licenses/mit-license.php)
  */
 `
-
-const packagePath = path.join(__dirname, 'package.json')
-
-fs.readFileSync(packagePath, 'utf8', (err, data) => {
-  if (err) {
-    console.log(err)
-  } else {
-    const obj = JSON.parse(data)
-    obj.version = version
-    const json = JSON.stringify(obj, null, 2)
-    fs.writeFile(packagePath, `${json} \n`, 'utf8')
-  }
-})
 
 export default [
   {
